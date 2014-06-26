a018ed47fce93f5fbc8e2b5fdbcc64da6c49155a
Synopticum <synopticum@gmail.com>
2014-06-26 17:08:07 +0400

    Initial commit

---
+++ js/call.js
@@ -0,0 +1,83 @@
+$( ".buttons .button span" ).hover(
+  function() {
+    $(this).siblings(".notice").toggle();
+  }
+);
+
+// toggle visibility
+jQuery.fn.visible = function() {
+    return this.css('visibility', 'visible');
+};
+
+jQuery.fn.invisible = function() {
+    return this.css('visibility', 'hidden');
+};
+
+jQuery.fn.visibilityToggle = function() {
+    return this.css('visibility', function(i, visibility) {
+        return (visibility == 'visible') ? 'hidden' : 'visible';
+    });
+};
+
+$( ".header-updates" ).click(
+  function() {
+    $(".customscroll", this).visibilityToggle();
+  }
+);
+
+!(function($) {
+    var toggle = $.fn.toggle;
+    $.fn.toggle = function() {
+        var args = $.makeArray(arguments),
+            lastArg = args.pop();
+
+        if (lastArg == 'visibility') {
+            return this.visibilityToggle();
+        }
+
+        return toggle.apply(this, arguments);
+    };
+})(jQuery);
+
+// custom scrollbar
+window.onload = function() {
+    window.dima = baron({
+        root: '.wrapper_1',
+        scroller: '.scroller',
+        bar: '.scroller__bar',
+        barOnCls: 'baron'
+    }).fix({
+        elements: '.header__title',
+        outside: 'header__title_state_fixed',
+        before: 'header__title_position_top',
+        after: 'header__title_position_bottom',
+        clickable: true
+    }).pull({
+        block: '.load',
+        elements: [{
+            self: '.load__value',
+            property: 'width'
+        }],
+        limit: 115,
+        onExpand: function() {
+            $('.load').css('background', 'red');
+        }
+    });
+
+    baron({
+        scroller: '.wrapper_2 .scroller',
+        bar: '.scroller__bar',
+        barOnCls: 'baron'
+    }).fix({
+        elements: '.header__title',
+        outside: 'header__title_state_fixed',
+        before: 'header__title_position_top',
+        after: 'header__title_position_bottom'
+    }).baron({
+        barOnCls: 'baron_h',
+        bar: '.scroller__bar_h'
+    });
+};
+
+// fancybox
+$('.fancybox').fancybox();
